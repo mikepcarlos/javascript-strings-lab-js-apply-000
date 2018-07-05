159320be539487f6a1ce121de2ed5cc6d3c29779
Michael Carlos <michaelpcarlos@gmail.com>
2018-07-05 19:40:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var greeting = 'Hello, everybody!';
 var specialGuest = 'Drake';
 
 var greetSpecialGuest = `Hello, ${specialGuest}!`;
+
+var topic = 'hits';
+
+var conversation = `Oh look! it's ${specialGuest}. Man of ${topic}!`;
